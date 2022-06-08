aba9f4fb991278b576bf59e4e1a34d5ab41af7aa
Chienguin <fcnguyen2@gmail.com>
2022-06-08 00:02:39 -0500

    Updated script to search through all Sharepoint Sites
    Initial script only searched through Joined Teams. Script should now search through all Sharepoint Sites and not just Teams

--- AzAutoMapper/run.ps1
+++ AzAutoMapper/run.ps1
@@ -3,36 +3,34 @@ param($Request, $TriggerMetadata)
 #
 #
 $TenantID = $Request.Query.TenantID
-$user = $Request.Query.Username
+$User = $Request.Query.Username
  
-$body = @{
+$Body = @{
     'resource'      = 'https://graph.microsoft.com'
     'client_id'     = $ENV:ApplicationId
     'client_secret' = $ENV:ApplicationSecret
     'grant_type'    = "client_credentials"
     'scope'         = "openid"
 }
-$ClientToken = Invoke-RestMethod -Method post -Uri "https://login.microsoftonline.com/$($tenantid)/oauth2/token" -Body $body -ErrorAction Stop
-$headers = @{ "Authorization" = "Bearer $($ClientToken.access_token)" }
-$UserID = (Invoke-RestMethod -Uri "https://graph.microsoft.com/beta/users/$($user)" -Headers $Headers -Method Get -ContentType "application/json").id
+$ClientToken = Invoke-RestMethod -Method POST -Uri "https://login.microsoftonline.com/$($TenantID)/oauth2/token" -Body $Body -ErrorAction Stop
+$Headers = @{ "Authorization" = "Bearer $($ClientToken.access_token)" }
+$UserID = (Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/users/$($User)" -Headers $Headers -Method GET -ContentType "application/json").email
  
-$AllTeamsURI = "https://graph.microsoft.com/beta/users/$($UserID)/JoinedTeams"
-$Teams = (Invoke-RestMethod -Uri $AllTeamsURI -Headers $Headers -Method Get -ContentType "application/json").value
-$MemberOf = foreach ($Team in $Teams) {
-    $SiteRootUri = "https://graph.microsoft.com/beta/groups/$($Team.id)/sites/root"
-    $SiteRootReq = Invoke-RestMethod -Uri $SiteRootUri  -Headers $Headers -Method Get -ContentType "application/json"
-    $SiteDrivesUri = "https://graph.microsoft.com/beta/groups/$($Team.id)/sites/root/Lists"
-    $SitesDrivesReq = (Invoke-RestMethod -Uri $SiteDrivesUri -Headers $Headers -Method Get -ContentType "application/json").value | where-object { $_.Name -eq "Shared Documents" }
-    $DriveInfo = $SitesDrivesReq.ParentReference.siteid -split ','
-    if ($SiteRootReq.description -like "*no-auto-map*") { continue }
+$AllSitesURI = "https://graph.microsoft.com/v1.0/sites?search=$($UserID)"
+$Sites = (Invoke-RestMethod -Uri $AllSitesURI -Headers $Headers -Method GET -ContentType "application/json").value
+$MemberOf = foreach ($Site in $Sites) {
+    $SiteWebID = $Site.id -split ','
+    $SiteDrivesUri = "https://graph.microsoft.com/v1.0/sites/$($SiteWebID[1])/lists"
+    $SitesDrivesReq = (Invoke-RestMethod -Uri $SiteDrivesUri -Headers $Headers -Method GET -ContentType "application/json").value | where-object { $_.Name -eq "Shared Documents" }
+    if ($Site.description -like "*no-auto-map*") { continue }
     if ($null -eq [System.Web.HttpUtility]::UrlEncode($SitesDrivesReq.id)) { continue }
     [pscustomobject] @{
-        SiteID    = [System.Web.HttpUtility]::UrlEncode("{$($DriveInfo[1])}")
-        WebID     = [System.Web.HttpUtility]::UrlEncode("{$($DriveInfo[2])}")
+        SiteID    = [System.Web.HttpUtility]::UrlEncode("{$($SiteWebID[1])}")
+        WebID     = [System.Web.HttpUtility]::UrlEncode("{$($SiteWebID[2])}")
         ListID    = [System.Web.HttpUtility]::UrlEncode("{$($SitesDrivesReq.id)}")
-        WebURL    = [System.Web.HttpUtility]::UrlEncode($SiteRootReq.webUrl)
-        Webtitle  = [System.Web.HttpUtility]::UrlEncode($($Team.displayName)).Replace("+", "%20")
-        listtitle = [System.Web.HttpUtility]::UrlEncode($SitesDrivesReq.name)
+        WebURL    = [System.Web.HttpUtility]::UrlEncode($Site.webUrl)
+        WebTitle  = [System.Web.HttpUtility]::UrlEncode($($Site.Name)).Replace("+", "%20")
+        ListTitle = [System.Web.HttpUtility]::UrlEncode($SitesDrivesReq.name)
     }
  
 }
